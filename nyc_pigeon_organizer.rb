e17f74eafdf2075806b8c55369686effce755fb3
Donny L <donny.landis@gmail.com>
2020-02-24 03:03:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pd)
+  org_pigs = {}
+  t_and_a = build_t_a_lookup(pd)
+  pd.each_pair do |trait, attrib|
+    attrib.each_pair do |value, name_array|
+      name_array.each do |name|
+        if !org_pigs[name]
+          org_pigs[name] = {}
+        end
+        if !org_pigs[name][trait]
+          org_pigs[name][trait] = [value.to_s]
+        else
+          org_pigs[name][trait] << value.to_s
+        end
+      end
+    end
+  end
+  p org_pigs
 end
